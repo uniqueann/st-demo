ea379d5721b006d3a27e858c8930846a5cdda868
ai_ningning <uniqueann@163.com>
2024-12-27 16:22:32 +0800

    Update 10_📄_siliconflow.py
    text.list().data

--- pages/10_📄_siliconflow.py
+++ pages/10_📄_siliconflow.py
@@ -209,7 +209,7 @@ with tabModels:
     url = base_url + "/models"
     headers = {"Authorization": "Bearer "+openai.api_key}
     response = requests.request("GET", url, headers=headers)
-    data = response.text.data
+    data = response.text.list().data
     st.write(data)
     models = [x["id"] for x in data]
     st.write(models)
