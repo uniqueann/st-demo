3534f7c9089c8d53d0d937210359937a65cb8830
ai_ningning <uniqueann@163.com>
2025-01-15 18:44:33 +0800

    Update 2_excel.py
    default layout=wide

--- pages/2_excel.py
+++ pages/2_excel.py
@@ -2,6 +2,8 @@ import streamlit as st
 import pandas as pd
 from st_aggrid import GridOptionsBuilder,AgGrid
 
+st.set_page_config(layout="wide")
+
 st.subheader('仅读取第一个sheet表',divider=True)
 uploaded_file = st.file_uploader('请上传 XLSX 文件',type=['xlsx'])
 
