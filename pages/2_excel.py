a9170f9078c6436b2910cf043dc8287472d43f94
ai_ningning <uniqueann@163.com>
2025-01-15 18:05:21 +0800

    Update 2_excel.py

--- pages/2_excel.py
+++ pages/2_excel.py
@@ -23,21 +23,22 @@ with st.container():
 
 tabs = st.tabs(sheet_selects)
 
+with tabs[0]:
+    st.dataframe(dfs[0])
 
-
-for tab,name in zip(tabs,sheet_selects):
-    with tab:
-        df = dfs[name]
-        gb = GridOptionsBuilder.from_dataframe(df)
-        gb.configure_side_bar()
-        gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=True)
-        gridOptions = gb.build()
-        # st.dataframe(df)
-        AgGrid(
-            pd.DataFrame(df, columns=df.columns),
-            gridOptions=gridOptions,
-            fit_columns_on_grid_load=True,
-            height=800,
-            editable=True,
-            enable_enterprise_modules=True
-        )
+# for tab,name in zip(tabs,sheet_selects):
+#     with tab:
+#         df = dfs[name]
+#         gb = GridOptionsBuilder.from_dataframe(df)
+#         gb.configure_side_bar()
+#         gb.configure_default_column(groupable=True, value=True, enableRowGroup=True, aggFunc='sum', editable=True)
+#         gridOptions = gb.build()
+#         # st.dataframe(df)
+#         AgGrid(
+#             pd.DataFrame(df, columns=df.columns),
+#             gridOptions=gridOptions,
+#             fit_columns_on_grid_load=True,
+#             height=800,
+#             editable=True,
+#             enable_enterprise_modules=True
+#         )
